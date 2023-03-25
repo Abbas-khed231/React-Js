1126807b366d0ae83608733c2ccba143592cb289
Abbas-khed231 <khedabbas@gmail.com>
2023-03-25 12:47:30 +0530

    Component Demo

---
+++ learning/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Slider from './components/Slider';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ learning/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+    render(){
+        return (
+            <>
+                <div>Header</div>
+                <div>
+                    <h1>Test</h1>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Header;
